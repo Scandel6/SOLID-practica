e1ebd678ab266c67abf5c11a8f7324b2d07e3f9e
Salvador Candel <salvacandel@gmail.com>
2022-04-24 20:56:14 +0200

    Applying Single responsibility principle to File

---
+++ src/com/kreitek/service/Converter.java
@@ -0,0 +1,44 @@
+package com.kreitek.service;
+
+import com.kreitek.files.File;
+import com.kreitek.files.FileSystemItem;
+import com.kreitek.files.error.InvalidFileFormatException;
+
+public class Converter {
+
+    public static FileSystemItem convertMp3ToWav(File file) {
+        if (!"mp3".equalsIgnoreCase(file.getExtension())) {
+            throw new InvalidFileFormatException("El fichero debe ser mp3");
+        }
+
+        int indexOfLastDot = file.getName().lastIndexOf(".");
+        String nameWithoutExtension = file.getName();
+        if (indexOfLastDot > 0) {
+            nameWithoutExtension = file.getName().substring(0, indexOfLastDot);
+        }
+        String newFileName = nameWithoutExtension + ".wav";
+        FileSystemItem result = new File(file.getParent(), newFileName);
+        result.open();
+        // Lógica de conversión de mp3 a wav. Se lee de este fichero y se escribe en result
+        result.close();
+        return result;
+    }
+
+    public static FileSystemItem convertWavToMp3(File file) {
+        if (!"wav".equalsIgnoreCase(file.getExtension())) {
+            throw new InvalidFileFormatException("El fichero debe ser wav");
+        }
+
+        int indexOfLastDot = file.getName().lastIndexOf(".");
+        String nameWithoutExtension = file.getName();
+        if (indexOfLastDot > 0) {
+            nameWithoutExtension = file.getName().substring(0, indexOfLastDot);
+        }
+        String newFileName = nameWithoutExtension + ".mp3";
+        FileSystemItem result = new File(file.getParent(), newFileName);
+        result.open();
+        // Lógica de conversión de wav a mp3. Se lee de este fichero y se escribe en result
+        result.close();
+        return result;
+    }
+}
