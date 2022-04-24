250925c6372232f099121bbbfcce125300cacc84
Salvador Candel <salvacandel@gmail.com>
2022-04-24 20:23:59 +0200

    Aplying Open Closed principle in FileManager

--- src/com/kreitek/service/FileManager.java
+++ src/com/kreitek/service/FileManager.java
@@ -1,7 +1,6 @@
 package com.kreitek.service;
 
-import com.kreitek.files.Directory;
-import com.kreitek.files.File;
+
 import com.kreitek.files.FileSystemItem;
 
 import java.util.List;
@@ -9,31 +8,7 @@ import java.util.List;
 public class FileManager {
 
     public static int calculateSize(FileSystemItem fileSystemItem) {
-        int totalSize = 0;
-
-        if (fileSystemItem instanceof File) {
-            totalSize = fileSystemItem.getSize();
-        } else if (fileSystemItem instanceof Directory) {
-            for (FileSystemItem item : fileSystemItem.listFiles()) {
-                totalSize += calculateSize(item);
-            }
-        }
-
-        return totalSize;
-    }
-
-    public static int calculateSize(List<FileSystemItem> files) {
-        int totalSize = 0;
-
-        for(FileSystemItem item : files) {
-            if (item instanceof File) {
-                totalSize += item.getSize();
-            } else if (item instanceof Directory) {
-                totalSize += calculateSize(item.listFiles());
-            }
-        }
-
-        return totalSize;
+        return fileSystemItem.getSize();
     }
 
     // Aquí habría otros métodos para gestionar ficheros y directorios:
